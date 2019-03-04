2a5d867059ee854012358c78765b466f1969e199
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2019-03-04 18:29:18 +0900

    Route

--- Component/App/route.js
+++ Component/App/route.js
@@ -1,75 +1,69 @@
-import React from "react";
-import { Platform, StatusBar } from "react-native";
-import {
-  createStackNavigator,
-  createBottomTabNavigator,
-  createSwitchNavigator
-} from "react-navigation";
-import { FontAwesome } from "react-native-vector-icons";
-import SignUp from "../../Route/Login/SignUp";
-import SignIn from "../../Route/Login/SignIn";
-import Home from "../../Route/Login/Home";
-import Profile from "../../Route/Login/Profile";
-import tabNavigation from "../Navigation/AppNavigation";
-import PictureNavigation from "../Navigation/PictureNavigation";
+// import { Platform, StatusBar } from "react-native";
+// import { createStackNavigator, createSwitchNavigator } from "react-navigation";
+// import SignUp from "../../Route/Login/SignUp";
+// import SignIn from "../../Route/Login/SignIn";
+// import tabNavigation from "../Navigation/AppNavigation";
+// import PictureNavigation from "../Navigation/PictureNavigation";
 
-const headerStyle = {
-  marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
-};
+// const headerStyle = {
+//   marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+// };
 
-export const SignedOut = createStackNavigator({
-  SignUp: {
-    screen: SignUp,
-    navigationOptions: {
-      title: "Sign Up",
-      headerStyle
-    }
-  },
-  SignIn: {
-    screen: SignIn,
-    navigationOptions: {
-      title: "Sign In",
-      headerStyle
-    }
-  }
-});
+// export const SignedOut = createStackNavigator({
+//   SignUp: {
+//     screen: SignUp,
+//     navigationOptions: {
+//       title: "Sign Up",
+//       headerStyle
+//     }
+//   },
+//   SignIn: {
+//     screen: SignIn,
+//     navigationOptions: {
+//       title: "Sign In",
+//       headerStyle
+//     }
+//   }
+// });
 
-export const SignedIn = createStackNavigator(
-  {
-    Tabs: {
-      screen: tabNavigation,
-      navigationOptions: {
-        header: null
-      }
-    },
-    TakePhoto: {
-      screen: PictureNavigation,
-      navigationOptions: {
-        header: null
-      }
-    }
-  },
-  {
-    tabBarOptions: {
-      style: {
-        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
-      }
-    }
-  }
-);
+// export const SignedIn = createStackNavigator(
+//   {
+//     Tabs: {
+//       screen: tabNavigation,
+//       navigationOptions: {
+//         header: null,
+//         headerStyle
+//       }
+//     },
+//     TakePhoto: {
+//       screen: PictureNavigation,
+//       navigationOptions: {
+//         title: "Photo",
+//         headerStyle
+//       }
+//     }
+//   },
+//   {
+//     tabBarOptions: {
+//       style: {
+//         paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+//       }
+//     }
+//   }
+// );
 
-export const createRootNavigator = (signedIn = false) => {
-  return createSwitchNavigator(
-    {
-      SignedIn: {
-        screen: SignedIn
-      },
-      SignedOut: {
-        screen: SignedOut
-      }
-    },
-    {
-      initialRouteName: signedIn ? "SignedIn" : "SignedOut"
-    }
-  );
-};
+// export const createRootNavigator = (signedIn = false) => {
+//   return createSwitchNavigator(
+//     {
+//       SignedIn: {
+//         screen: SignedIn
+//       },
+//       SignedOut: {
+//         screen: SignedOut
+//       }
+//     },
+//     {
+//       initialRouteName: signedIn ? "SignedIn" : "SignedOut"
+//     }
+//   );
+// };
